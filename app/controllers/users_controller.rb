1b898d8e20825a8b69ec6c410300455f720aaa6a
Hector F <hectorf.@Hectors-MacBook-Pro.local>
2018-11-21 01:28:37 -0500

    validate series title

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  def index
+    users = User.all
+    render json: users
+  end
+  
   # GET /users/new
   def new
     @user = User.new
