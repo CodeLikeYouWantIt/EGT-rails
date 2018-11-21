39ba34c87816b68bceeab20715cd633afb17f1e7
Hector F <hectorf.@Hectors-MacBook-Pro.local>
2018-11-21 01:05:23 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post   'login'   => 'sessions#create'
-  resources :users, only: [:new, :create, :update, :destroy]
+  resources :users, only: [:index, :new, :create, :update, :destroy]
+  resources :series, only [:index, :new, :create, :update, :destroy]
+  resources :posts, only [:index, :new, :create, :update, :destroy]
+  resources :comments, only [:index, :new, :create, :update, :destroy]
 end
